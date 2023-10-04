333a88adec0536ea2e132a89f057521e721c21f5
TwoFacedToby <tobiasgoerlyk@hotmail.com>
2023-10-04 17:21:36 +0200

    Fixed the Gradle Stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,15 +50,23 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.compose.ui:ui:1.1.0-alpha01")  // Replace with the latest version
+    implementation("androidx.compose.material:material:1.1.0-alpha01")  // Replace with the latest version
+    implementation("androidx.compose.runtime:runtime:1.1.0-alpha01")  // Replace with the latest version
+    implementation("androidx.compose.material3:material3:1.1.0-alpha01")  // Replace with the latest version
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,14 +75,4 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    //Added for this project:
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.compose.ui:ui:1.6.0-alpha06")
-    implementation("androidx.compose.material:material:1.6.0-alpha06")
-    implementation("androidx.compose.runtime:runtime:1.6.0-alpha06")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha08")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 }
