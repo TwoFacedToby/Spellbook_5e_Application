4fe9ef3022e6131c24332e86bafb2e424a6f52f4
mikkelJurs <s224279@student.dtu.dk>
2023-10-29 20:48:40 +0100

    removed unnecessary comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,10 +52,10 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.1.0-alpha01")  // Replace with the latest version
-    implementation("androidx.compose.material:material:1.1.0-alpha01")  // Replace with the latest version
-    implementation("androidx.compose.runtime:runtime:1.1.0-alpha01")  // Replace with the latest version
-    implementation("androidx.compose.material3:material3:1.1.0-alpha01")  // Replace with the latest version
+    implementation("androidx.compose.ui:ui:1.1.0-alpha01")
+    implementation("androidx.compose.material:material:1.1.0-alpha01")
+    implementation("androidx.compose.runtime:runtime:1.1.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha01")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
