78bbdfc577592c20014e1621ee6cebeecf2b713d
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2023-11-02 15:55:42 +0100

    Merged Toby's API and the current UI into this branch

--- app/src/main/java/com/example/spellbook5eapplication/MainActivity.kt
+++ app/src/main/java/com/example/spellbook5eapplication/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.spellbook5eapplication.app.view.MainScreen
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.spellbook5eapplication.app.Model.Data_Model.Filter
 import com.example.spellbook5eapplication.app.Model.Data_Model.SpellList
 import com.example.spellbook5eapplication.app.Utility.SpellController
@@ -24,17 +17,16 @@ class MainActivity : ComponentActivity() {
     val scope = CoroutineScope(Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            Spellbook5eApplicationTheme {
-                MainScreen()
-            }
-        }
         scope.launch {
             runBlocking {
                 networkRequest { spellList ->
                     spellList.printInfoToConsole()
                 }
+            }
+        }
+        setContent {
+            Spellbook5eApplicationTheme {
+                MainScreen()
             }
         }
     }
@@ -53,20 +45,4 @@ fun exampleFilter() : Filter{
     val filter = Filter()
     filter.setSpellName("Fire")
     return filter
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Spellbook5eApplicationTheme {
-        Greeting("Android")
-    }
 }
