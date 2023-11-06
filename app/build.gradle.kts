05259cd13e8c3d93490e4cfd111c1b577a5a5235
mikkelJurs <s224279@student.dtu.dk>
2023-11-06 12:25:27 +0100

    Added dependencies for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.8.5")
     implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
