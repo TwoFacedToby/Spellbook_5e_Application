36e1d835ab023a752fec0ec6089cd03a53bde724
Nicklas <Nicklas-Christensen@outlook.dk>
2023-11-06 10:00:43 +0100

    Delete overlay is now Dialog type, allowing it to be more pop-up like

--- app/src/main/java/com/example/spellbook5eapplication/app/view/Overlays/DeletePrompt.kt
+++ app/src/main/java/com/example/spellbook5eapplication/app/view/Overlays/DeletePrompt.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -32,54 +34,80 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.spellbook5eapplication.R
 import com.example.spellbook5eapplication.app.view.spellCards.LocalLargeSpellCardOverlay
 import com.example.spellbook5eapplication.app.view.utilities.ColouredButton
+import com.example.spellbook5eapplication.app.view.utilities.CreateDialog
 import com.example.spellbook5eapplication.app.viewmodel.GlobalOverlayState
 import kotlinx.coroutines.launch
 
 @Composable
 fun DeleteOverlay(onDismissRequest: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Are you sure you want to delete this spell?", color = colorResource(id = R.color.white), fontSize = 17.sp, fontWeight = FontWeight.Bold)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-
-        ColouredButton(
-            "Cancel", modifier = Modifier, color = ButtonDefaults.buttonColors(
-                containerColor = colorResource(
-                    id = androidx.appcompat.R.color.material_grey_800
+    Dialog(
+        onDismissRequest = { /*TODO*/ }
+    )
+    {
+        Box(
+            modifier = Modifier
+                .size(width = 350.dp, height = 100.dp)
+                .background(
+                    color = colorResource(id = R.color.main_color),
+                    shape = RoundedCornerShape(15.dp)
                 )
-            )
-        ) {
-            println("Button dismiss clicked")
-        }
-
-            Spacer(modifier = Modifier.width(30.dp))
-
-        ColouredButton(
-            "Delete", modifier = Modifier, color = ButtonDefaults.buttonColors(
-                containerColor = colorResource(
-                    id = R.color.red_button
+                .border(
+                    width = 2.dp,
+                    color = colorResource(id = R.color.border_color),
+                    shape = RoundedCornerShape(15.dp)
                 )
-            )
         ) {
-            println("Button delete clicked")
-        }
-    }
+            Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top) {
 
+                Text(
+                    modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+                    text = "Are you sure you want to delete this spell?",
+                    color = colorResource(id = R.color.white),
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+
+                    ColouredButton(
+                        "Cancel", modifier = Modifier, color = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(
+                                id = androidx.appcompat.R.color.material_grey_800
+                            )
+                        )
+                    ) {
+                        println("Button dismiss clicked")
+                    }
+
+                    Spacer(modifier = Modifier.width(30.dp))
+
+                    ColouredButton(
+                        "Delete", modifier = Modifier, color = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(
+                                id = R.color.red_button
+                            )
+                        )
+                    ) {
+                        println("Button delete clicked")
+                    }
+                }
+            }
+        }
     }
 }
+
 
 @Preview
 @Composable
