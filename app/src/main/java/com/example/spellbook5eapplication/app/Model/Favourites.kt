36121bc18cf2ac415a06ce3654a7cfe863303974
mikkelJurs <s224279@student.dtu.dk>
2023-11-13 11:38:56 +0100

    Created the basic functionality of creating a list, adding to it and saving to it. Have to be saved statically on the device as json.

--- app/src/main/java/com/example/spellbook5eapplication/app/Model/Favourites.kt
+++ app/src/main/java/com/example/spellbook5eapplication/app/Model/Favourites.kt
@@ -1,5 +1,18 @@
 package com.example.spellbook5eapplication.app.Model
 
 class Favourites {
+    var favouriteList = mutableListOf<String>()
 
+    fun addFavourite(spellname : String) {
+        favouriteList.add(spellname)
+    }
+
+    fun removeFavourite(spellname : String) {
+        val wasRemoved = favouriteList.remove(spellname)
+        if (wasRemoved) {
+            println("$spellname removed from favourites")
+        } else {
+            println("$spellname not found in favourites")
+        }
+    }
 }
