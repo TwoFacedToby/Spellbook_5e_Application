5d4179fc764342f486205727a29db585d8ff8822
mikkelJurs <s224279@student.dtu.dk>
2023-11-13 11:41:46 +0100

    Created method for adding the FavouritesList to local storage as Json.

--- app/src/main/java/com/example/spellbook5eapplication/app/Model/Favourites.kt
+++ app/src/main/java/com/example/spellbook5eapplication/app/Model/Favourites.kt
@@ -1,5 +1,8 @@
 package com.example.spellbook5eapplication.app.Model
 
+import com.example.spellbook5eapplication.app.Utility.SpellController
+import com.google.gson.Gson
+
 class Favourites {
     var favouriteList = mutableListOf<String>()
 
@@ -13,6 +16,15 @@ class Favourites {
             println("$spellname removed from favourites")
         } else {
             println("$spellname not found in favourites")
+        }
+    }
+
+    fun saveSpellbookToFile(spellBookName: String) {
+        favouriteList?.let {
+            val gson = Gson()
+            val favouritesListGson = gson.toJson(it)
+
+            SpellController.saveJsonToFile(favouritesListGson, "Favourites", spellBookName + ".json")
         }
     }
 }
