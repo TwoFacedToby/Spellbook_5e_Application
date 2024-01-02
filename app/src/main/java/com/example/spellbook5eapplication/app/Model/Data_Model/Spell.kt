3cb9859cf9a004cede11deb2e0e1b43ce0e121de
TobiasPedersenGoerlyk <tobiasgoerlyk@hotmail.com>
2024-01-02 15:14:43 +0100

    Added Spell

---
+++ app/src/main/java/com/example/spellbook5eapplication/app/Model/Data_Model/Spell.kt
@@ -0,0 +1,123 @@
+package com.example.spellbook5eapplication.app.Model.Data_Model
+
+class Spell  {
+    var index: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var name: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var desc: List<String> = listOf()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var atHigherLevel: List<String> = listOf()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var range: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var components: List<String> = listOf()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var materials: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var ritual: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var duration: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var concentration: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
+    var casting_time: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var level: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var school: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var classes: List<String> = listOf()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var url: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var attackType: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var damage: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var dc: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var aoe: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+    var homebrew: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+}
