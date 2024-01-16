d63ec69de6709d08355df704078190612bcdc76d
Kenneth Kaiser <kk1995@live.dk>
2024-01-16 14:22:13 +0100

    Toast for add

--- app/src/main/java/com/example/spellbook5eapplication/app/view/spellCards/NewSpellCard.kt
+++ app/src/main/java/com/example/spellbook5eapplication/app/view/spellCards/NewSpellCard.kt
@@ -2,6 +2,8 @@ package com.example.spellbook5eapplication.app.view.spellCards
 
 import SpellQueryViewModel
 import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -35,6 +37,7 @@ import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -56,6 +59,7 @@ import com.example.spellbook5eapplication.R
 import com.example.spellbook5eapplication.app.Model.Data_Model.Spell
 import com.example.spellbook5eapplication.app.Model.Data_Model.Spellbook
 import com.example.spellbook5eapplication.app.Repository.SpellbookManager
+import com.example.spellbook5eapplication.app.Utility.LocalDataLoader
 import com.example.spellbook5eapplication.app.viewmodel.GlobalOverlayState
 import com.example.spellbook5eapplication.app.viewmodel.OverlayType
 import kotlinx.coroutines.CoroutineScope
@@ -446,6 +450,9 @@ fun SelectSpellbookDialog(spellbooks: List<Spellbook>, onDismiss: () -> Unit, sp
 
                             spellbooks[index].addSpellToSpellbook(spell.index ?: "")
                             SpellbookManager.saveSpellbookToFile(spellbooks[index].spellbookName)
+                            LocalDataLoader.getContext()?.get()?.let { context ->
+                                Toast.makeText(context, "${spell.name}" + " added to " +"${spellbooks[index].spellbookName}", Toast.LENGTH_SHORT).show()
+                            }
                             onDismiss()
                         }
                     )
